90ba92a71e2577d982c851be60eca216875cce1c
Sergey Gan <googlgan@gmail.com>
2021-04-07 19:55:01 +0300

    HomeWork7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
+import handler.TestHandler;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        TestHandler.start(TestClass.class);
     }
 }
